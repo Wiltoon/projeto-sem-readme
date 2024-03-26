d8f1502c6b60751a0cfc26cafdf9c13cbb17b8f2
Wilton <wiltoon>
2024-03-25 20:03:51 -0400

    Alteração do Joao

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Uma outra alteração!</Text>
+      <Text>Texto do Joao!</Text>
       <StatusBar style="auto" />
     </View>
   );
